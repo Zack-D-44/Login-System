d9985e8e86a5ffc3c153b52a4f04320be3dd3852
zackd44 <zackdavison1@outlook.com>
2024-07-24 15:19:31 +0100

    Created Router

--- client/login-system/src/index.js
+++ client/login-system/src/index.js
@@ -2,13 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+
+const router = createBrowserRouter([{
+  path: '/',
+  element: <App />
+}
+
+])
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <RouterProvider router={router} />
 );
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
